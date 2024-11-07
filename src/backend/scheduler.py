72887443669a2377051493b7baf1e5680091484a
AlexV531 <v.alexander.531@gmail.com>
2024-11-07 15:26:41 -0500

    Started writing scheduler algorithm script.

---
+++ src/backend/scheduler.py
@@ -0,0 +1,6 @@
+
+games = [] # get this from gen_sched_input.py
+
+# Recusive function that takes an index from the games list that refers to a game
+def backtrack_scheduler(curr_game: int):
+    pass
