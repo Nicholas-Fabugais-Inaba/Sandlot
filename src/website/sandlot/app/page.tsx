e5c7ab352122c10e571a266b0b8c4c8d58b209e6
casra <casra02@gmail.com>
2024-11-10 23:01:46 -0500

    setting up passing useState to clanedar component (to pass schedule to calendar component

--- src/website/sandlot/app/page.tsx
+++ src/website/sandlot/app/page.tsx
@@ -5,12 +5,23 @@ import styles from './styles.module.css'
 import Calendar from "@/components/calendar";
 import getSchedule from '../functions/getSchedule'
 
+import { useState } from 'react';
+
 export default function Home() {
+  const [games, setGames] = useState([]);
+  console.log("games:");
+  console.log(games)
+
   return (
     <div className={styles.container}>
-      <div className={styles.button} onClick={getSchedule}>Generate Schedule</div>
+      <div 
+        className={styles.button} 
+        onClick={async () => {setGames(await getSchedule())}}
+      >
+          Generate Schedule
+      </div>
       <div className={styles.calendar}>
-        <Calendar></Calendar>
+        <Calendar games={games}></Calendar>
       </div>
     </div>
   );
--- src/website/sandlot/components/calendar.tsx
+++ src/website/sandlot/components/calendar.tsx
@@ -3,24 +3,28 @@ import dayGridPlugin from '@fullcalendar/daygrid' // might be unneccesary packag
 import timeGridPlugin from '@fullcalendar/timegrid' // a plugin
 import styles from '../app/styles.module.css'; // Import the CSS module
 
-export default function Calendar() {
-  const games = [
-    {
-      title: 'Game: Tigers vs Dodgers',
-      start: '2024-11-08T17:00:00',
-      end: '2024-11-08T18:30:00',
-    },
-    {
-      title: 'Game: Mets vs Yankees',
-      start: '2024-11-08T17:00:00',
-      end: '2024-11-08T18:30:00',
-    },
-    {
-      title: 'Game: Cubs vs Red Sox',
-      start: '2024-11-08T17:00:00',
-      end: '2024-11-08T18:30:00',
-    },
-  ];
+import { useState } from 'react';
+
+export default function Calendar({ games }: any) {
+  const [events, setEvents] = useState(games);
+  
+  // const events = [
+  //   {
+  //     title: 'Game: Tigers vs Dodgers',
+  //     start: '2024-11-08T17:00:00',
+  //     end: '2024-11-08T18:30:00',
+  //   },
+  //   {
+  //     title: 'Game: Mets vs Yankees',
+  //     start: '2024-11-08T17:00:00',
+  //     end: '2024-11-08T18:30:00',
+  //   },
+  //   {
+  //     title: 'Game: Cubs vs Red Sox',
+  //     start: '2024-11-08T17:00:00',
+  //     end: '2024-11-08T18:30:00',
+  //   },
+  // ];
 
   return (
     <FullCalendar
@@ -32,7 +36,7 @@ export default function Calendar() {
         weekends={false}
         slotMinTime="13:00:00" // Slot starts at 1 PM
         slotMaxTime="23:59:00" // Slot ends at 11 PM
-        events={games}
+        events={events}
         eventClassNames={[styles.games]} // Apply the CSS module class to events
     />
   )
