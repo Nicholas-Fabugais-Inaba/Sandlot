3c6485e965fa9290608f7fc514c1e9e95beb56e9
casra <casra02@gmail.com>
2025-01-30 19:08:38 -0500

    initial db connectionn setup

---
+++ src/backend/app/db/models.py
@@ -0,0 +1,43 @@
+from typing import List
+from typing import Optional
+from sqlalchemy import ForeignKey
+from sqlalchemy import String
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+from sqlalchemy.orm import relationship
+
+import os
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+import urllib.parse
+
+load_dotenv()
+params = urllib.parse.quote_plus(os.getenv('CONN_STRING'))
+conn_str = 'mssql+pyodbc:///?odbc_connect={}'.format(params)
+engine = create_engine(conn_str,echo=True)
+
+class Base(DeclarativeBase):
+    pass
+
+class User(Base):
+    __tablename__ = "user_account"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(30))
+    fullname: Mapped[Optional[str]]
+    addresses: Mapped[List["Address"]] = relationship(
+        back_populates="user", cascade="all, delete-orphan"
+    )
+    def __repr__(self) -> str:
+        return f"User(id={self.id!r}, name={self.name!r}, fullname={self.fullname!r})"
+
+class Address(Base):
+    __tablename__ = "address"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    email_address: Mapped[str]
+    user_id: Mapped[int] = mapped_column(ForeignKey("user_account.id"))
+    user: Mapped["User"] = relationship(back_populates="addresses")
+    def __repr__(self) -> str:
+        return f"Address(id={self.id!r}, email_address={self.email_address!r})"
+    
+Base.metadata.create_all(engine)
