4d196dc4eb9bd91060f3c83120f07a2339753717
casra <casra02@gmail.com>
2025-01-30 19:24:45 -0500

    get rid of deprecated folders

--- src/backend/app/db/models.py
+++ src/backend/app/db/models.py
@@ -20,8 +20,8 @@ engine = create_engine(conn_str,echo=True)
 class Base(DeclarativeBase):
     pass
 
-class User(Base):
-    __tablename__ = "user_account"
+class Player(Base):
+    __tablename__ = "player"
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(String(30))
     fullname: Mapped[Optional[str]]
@@ -31,8 +31,17 @@ class User(Base):
     def __repr__(self) -> str:
         return f"User(id={self.id!r}, name={self.name!r}, fullname={self.fullname!r})"
 
-class Address(Base):
-    __tablename__ = "address"
+class Team(Base):
+    __tablename__ = "team"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    email_address: Mapped[str]
+    user_id: Mapped[int] = mapped_column(ForeignKey("user_account.id"))
+    user: Mapped["User"] = relationship(back_populates="addresses")
+    def __repr__(self) -> str:
+        return f"Address(id={self.id!r}, email_address={self.email_address!r})"
+    
+class Game(Base):
+    __tablename__ = "game"
     id: Mapped[int] = mapped_column(primary_key=True)
     email_address: Mapped[str]
     user_id: Mapped[int] = mapped_column(ForeignKey("user_account.id"))
