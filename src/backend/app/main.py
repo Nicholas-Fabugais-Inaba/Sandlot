5eb8971e893987ffd4623806258e8569a5a806d5
saltA-saurus <jungwoo114@gmail.com>
2025-02-24 19:36:14 -0500

    created basic team directory page

--- src/backend/app/main.py
+++ src/backend/app/main.py
@@ -2,7 +2,8 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from .routers.schedule_router import router as schedule_router
 from .routers.user_router import router as user_rotuer
-from .routers.standings_router import router as standings_router    
+from .routers.standings_router import router as standings_router   
+from .routers.teams_directory_router import router as teams_directory_router 
 
 
 app = FastAPI()
@@ -21,6 +22,7 @@ app.add_middleware(
 app.include_router(schedule_router, prefix="/schedule")
 app.include_router(user_rotuer, prefix="/user")
 app.include_router(standings_router, prefix="/standings")
+app.include_router(teams_directory_router, prefix="/team")
 
 
 @app.get("/")
