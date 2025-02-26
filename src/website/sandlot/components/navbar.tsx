0a8191c672bdc86534f0155b3060cb6c4d2d5805
Nicholas Fabugais-Inaba <nfabugaisinaba@gmail.com>
2025-02-26 14:31:59 -0500

    reduced window size navMenu now includes the navbar tabs; removed non-existent tabs

--- src/website/sandlot/components/navbar.tsx
+++ src/website/sandlot/components/navbar.tsx
@@ -102,7 +102,6 @@ export const Navbar = () => {
           {siteConfig.navMenuItems.map((item, index) => (
             <NavbarMenuItem key={`${item}-${index}`}>
               <Link
-                color={index === 2 ? "primary" : index === siteConfig.navMenuItems.length - 1 ? "danger" : "foreground"}
                 href={item.href}
                 size="lg"
               >
