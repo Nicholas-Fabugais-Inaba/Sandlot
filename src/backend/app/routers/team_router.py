aeab17caf0ad0108af3ef2e1f6dd48178afb0fce
saltA-saurus <jungwoo114@gmail.com>
2025-02-27 18:46:20 -0500

    added functionality so team directory gets division names from database

--- src/backend/app/routers/team_router.py
+++ src/backend/app/routers/team_router.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from ..db.queries import get_all_teams, get_team_info_by_current_user
+from ..db.queries import get_all_teams, get_team_info_by_current_user, get_division_name_by_division_id
 from .types import TeamID
 
 router = APIRouter(tags=["team"])
@@ -14,19 +14,6 @@ async def get_player_data(data: TeamID):
 async def get_team_data():
 
     teams = get_all_teams() # select(Team.id, Team.team_name, Team.division, Team.offday)
-    teams = {team['id']: {"id": team["id"], "name": team["team_name"], "division": map_division(team["division"])} for team in teams}
+    teams = {team['id']: {"id": team["id"], "name": team["team_name"], "division": get_division_name_by_division_id(team["division"])["division_name"]} for team in teams}
 
     return teams.values()
-
-# need to put into helper function file and use from there instead of repeating code with standings
-def map_division(divison_int):
-    if divison_int == 0:
-        return "Division A"
-    elif divison_int == 1:
-        return "Division B"
-    elif divison_int == 2:
-        return "Division C"
-    elif divison_int == 3:
-        return "Division D"
-    else:
-        return "Unknown"
