4719e8013fa166c4efb882b2d30d199700099cb7
saltA-saurus <jungwoo114@gmail.com>
2025-03-27 20:52:20 -0400

    cleared invalid username/password message upon resubmission of sign in

--- src/website/sandlot/app/account/signin/page.tsx
+++ src/website/sandlot/app/account/signin/page.tsx
@@ -19,6 +19,7 @@ export default function SignIn() {
 
   const handleSignIn = async (e: React.FormEvent) => {
     e.preventDefault();
+    setError(""); // Clear any previous error message
     const result = await signIn("credentials", {
       redirect: false, // Prevent automatic redirect
       userID,
