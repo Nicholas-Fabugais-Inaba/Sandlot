ea41ad45c1868905016117bbb931d40397d0ccf2
saltA-saurus <jungwoo114@gmail.com>
2025-03-27 20:00:04 -0400

    added dynamic sizing for height of team roster based on window height

--- src/website/sandlot/app/team/page.tsx
+++ src/website/sandlot/app/team/page.tsx
@@ -189,8 +189,8 @@ export default function TeamPage() {
             <h2 className="text-xl font-bold mb-2">
               {teamName || "Your Team"} Roster
             </h2>
-            <div className="max-h-80 overflow-y-auto p-2">
-              <Table
+            <div className="max-h-[calc(100vh-180px)] overflow-y-auto p-2"> {/* Added scrollable container */}
+            <Table
                 aria-label="Team Roster"
                 classNames={{ table: "min-w-full" }}>
                 <TableHeader>
@@ -263,7 +263,7 @@ export default function TeamPage() {
             <h2 className="text-xl font-bold mb-2">
               {teamName || "Your Team"} Roster
             </h2>
-            <div className="max-h-80 overflow-y-auto p-2">
+            <div className="max-h-[calc(100vh-180px)] overflow-y-auto p-2">
               <Table
                 aria-label="Team Roster"
                 classNames={{ table: "min-w-full" }}
