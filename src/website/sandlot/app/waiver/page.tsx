b84e4890f5ea2ae722992e0a71ab7d677627b772
saltA-saurus <jungwoo114@gmail.com>
2025-03-28 23:38:57 -0400

    moved edit and delete buttons to top

--- src/website/sandlot/app/waiver/page.tsx
+++ src/website/sandlot/app/waiver/page.tsx
@@ -266,21 +266,19 @@ export default function WaiverManagementPage() {
           <div className="mt-6">
             <h3 className="text-lg font-semibold mb-4">Waiver Sections</h3>
             {waiverFormat?.map((section) => (
-              <div key={section.id} className="flex items-center justify-between p-3 border rounded mb-2">
-                <div className="flex-1 mr-4 overflow-hidden">
-                  <p className="whitespace-pre-wrap break-words text-black dark:text-white"
+              <div key={section.id} className="flex items-start justify-between p-3 border rounded mb-2">
+              <div className="flex-1 mr-4 overflow-hidden">
+                <p className="whitespace-pre-wrap break-words text-black dark:text-white"
                   dangerouslySetInnerHTML={{
                     __html: decodeURIComponent(section.text).replace(/\n/g, "<br />"),
-                }}></p>
-                  {/* <span className="text-sm text-gray-500 dark:text-gray-400">
-                    {section.requireInitials ? 'Requires Initials' : 'No Initials Required'}
-                  </span> */}
-                </div>
-                <div className="flex space-x-2">
-                  <Button onPress={() => setEditingSection(section)}>Edit</Button>
-                  <Button onPress={() => deleteWaiverSection(section.id)}>Delete</Button>
-                </div>
+                  }}>
+                </p>
               </div>
+              <div className="flex space-x-2">
+                <Button onPress={() => setEditingSection(section)}>Edit</Button>
+                <Button onPress={() => deleteWaiverSection(section.id)}>Delete</Button>
+              </div>
+            </div>
             ))}
             <Button onPress={addWaiverSection} className="mt-4">Add Section</Button>
           </div>
