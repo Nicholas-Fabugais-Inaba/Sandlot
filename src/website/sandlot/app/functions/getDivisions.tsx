286abc30dee1502717cc508bf1aae88c4f857451
AlexV531 <v.alexander.531@gmail.com>
2025-03-30 04:09:04 -0400

    Added getDivisions backend connection.

---
+++ src/website/sandlot/app/functions/getDivisions.tsx
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+export default async function getDivisions(): Promise<any> {
+  const response = await axios.get(
+    `${process.env.NEXT_PUBLIC_APIHOST}/archive/get_divisions`,
+  );
+
+  console.log("getDivisions response:", response.data);
+
+  return response.data;
+}
