1951e6ac4acf27d1d1308cd24d1de9fd24753971
AlexV531 <v.alexander.531@gmail.com>
2025-03-30 04:10:18 -0400

    Updated getDivisions function.

--- src/website/sandlot/app/functions/getDivisions.tsx
+++ src/website/sandlot/app/functions/getDivisions.tsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default async function getDivisions(): Promise<any> {
   const response = await axios.get(
-    `${process.env.NEXT_PUBLIC_APIHOST}/archive/get_divisions`,
+    `${process.env.NEXT_PUBLIC_APIHOST}/season-setup/get_divisions`,
   );
 
   console.log("getDivisions response:", response.data);
